92e4d6b872ae3b09612afed6ddea5470ad9bc634
Chris Howe <chris@howeville.com>
2018-07-17 15:49:38 -0500

    initial commit

---
+++ ssss.cpp
@@ -0,0 +1,257 @@
+#include "ssss.h"
+#include <EEPROM.h>
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Lagrange Polynomial
+/////////////////////////////////////////////////////////////////////////////////////////////////
+gf256
+lagrange(int size, gf256 x[], int i, gf256 y) {
+  // evaluates the ith lagrange polynomial at y
+  // lagrange polynomial
+  // going through (x[i],1) and through each of the
+  // (x[j!=i],0) points.
+  
+  gf256 result = 1;
+
+  for(int j=0;j<size;++j) {
+    if(j != i) {
+      result = result * (x[j] - y) / (x[i] - x[j]);
+    }
+  }
+
+  return result;
+}
+
+void 
+Ssss::clear() {
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+  for(uint8_t i=0; i< SSSS_MAX_SHARES; i++ ) {
+    lagrange_value[i] = 0;
+    powers[i] = 0;
+  }
+}
+
+void Ssss::LoadState() {
+  mode = EEPROM.read(SSSS_EEPROM_MODE_IDX);
+  uint8_t f = EEPROM.read(SSSS_EEPROM_FAMILY_IDX);
+  family = f >> 3;
+  threshold = (f & 0x7) + 2;
+  shares = EEPROM.read(SSSS_EEPROM_SHARE_IDX);
+  payload = EEPROM.read(SSSS_EEPROM_PAYLOAD_IDX);
+}
+
+bool
+Ssss::hasFamily() const {
+  return shares > 1 || mode == Deal || mode == Reveal;
+}
+
+uint8_t
+Ssss::getMode() const {
+  return mode;
+}
+
+uint8_t
+Ssss::getFamilyId() const {
+  return family << 3 | ((threshold - 2) & 0x7);
+}
+
+uint8_t 
+Ssss::getShares() const {
+  return shares;
+}
+
+void 
+Ssss::setMode(uint8_t m) {
+  mode = m;
+  if(m != Shuffle) {
+    EEPROM.write(SSSS_EEPROM_MODE_IDX, mode);
+  }
+}
+
+void 
+Ssss::setFamily(uint8_t fam) {
+  family = fam;
+  //EEPROM.write( SSSS_EEPROM_FAMILY_IDX, getFamilyId());
+}
+
+void 
+Ssss::setThreshold(uint8_t thresh) {
+  threshold = thresh;
+  EEPROM.put( SSSS_EEPROM_FAMILY_IDX, getFamilyId());
+}
+
+void 
+Ssss::setPayload(uint8_t payl) {
+  payload = payl;
+  EEPROM.write( SSSS_EEPROM_PAYLOAD_IDX, payload);
+}
+
+void 
+Ssss::setPayloadLength(uint8_t payload) {
+  EEPROM.write( SSSS_EEPROM_PAYLOAD_IDX, payload);
+}
+
+
+  // Deal Setup
+void 
+Ssss::beginShuffle() {
+  mode = Shuffle;
+  ent_index = SSSS_EEPROM_DATA_START + payload;
+  ent_sweeps = 0;
+}
+
+bool 
+Ssss::addEntropy(uint8_t entropy) {
+  if(mode == Shuffle) {
+    if(ent_sweeps < 2) {
+      EEPROM.write(ent_index, entropy ^ EEPROM.read(ent_index));
+      ent_index += 1;
+      if(ent_index > SSSS_EEPROM_DATA_START + threshold * payload) {
+        ent_index = SSSS_EEPROM_DATA_START + payload;
+        ent_sweeps += 1;
+      }
+      return ent_sweeps >= 2;
+    }
+  }
+}
+
+void 
+Ssss::addSecret(uint8_t *secret) {
+  uint16_t index = SSSS_EEPROM_DATA_START;
+  for(uint8_t i = 0; i<payload; ++i) {
+    EEPROM.write(index++, secret[i]);
+    secret[i] = 0;  // dont leave extra copies of the secret lying around
+  }
+}
+
+
+void 
+Ssss::beginDeal() {
+  mode = Deal;
+
+  EEPROM.write(SSSS_EEPROM_MODE_IDX, mode);
+  EEPROM.write(SSSS_EEPROM_FAMILY_IDX, getFamilyId());
+  EEPROM.write(SSSS_EEPROM_PAYLOAD_IDX, payload);
+  shares = 0;
+  EEPROM.write(SSSS_EEPROM_SHARE_IDX, shares);
+}
+
+void 
+Ssss::dealNextShare(int8_t *buffer) { // writes share bytes to buffer - does not include checksum
+  shares++;
+
+  uint8_t i = 0;
+
+  buffer[i++] = getFamilyId();
+  buffer[i++] = shares;
+  gf256 gid = shares;
+
+  for(uint8_t j = 0; j<threshold; j++ ) {
+    powers[j] = gid.power(j);
+  }
+
+  for(uint8_t k =0; k<payload; k++) {
+    gf256 sum = 0;
+    uint16_t l = SSSS_EEPROM_DATA_START + k;
+    for(uint8_t j = 0; j<threshold; j++, l+=payload) {
+      sum = sum + powers[j] * EEPROM.read(l);
+    }
+    buffer[i++] = sum.n;
+  }
+
+  EEPROM.write( SSSS_EEPROM_SHARE_IDX, shares);
+}
+
+void 
+Ssss::beginCollect() {
+  mode = Collect;
+  //EEPROM.write(SSSS_EEPROM_MODE_IDX, mode);
+  shares = 0;
+  //EEPROM.write(SSSS_EEPROM_SHARE_IDX, shares);
+}
+
+
+bool
+Ssss::hasShare(uint8_t *share) const {
+  uint16_t p=SSSS_EEPROM_DATA_START;
+  for(uint8_t i=0; i<shares; i++, p+=payload+1) {
+    if(share[1] == EEPROM.read(p)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool
+Ssss::checkFamily(uint8_t *share) const {
+  return shares==0 || share[0] == getFamilyId();
+}
+
+void
+Ssss::addShare(uint8_t *share) {
+  uint8_t idx = shares;
+
+  if(shares == 0) {
+    setFamily( share[0] >> 3);
+    setThreshold( (share[0] & 0x7) + 2);
+  }
+  
+
+  // check for overwriting an existing share
+  uint16_t k = SSSS_EEPROM_DATA_START;
+  for(uint8_t j=0; j<idx; j++, k += payload + 1) {
+    if(EEPROM.read(k) == share[1]) {
+      idx = j;
+      break;
+    }
+  }
+
+  uint16_t p = payload + 1;
+  uint16_t j = SSSS_EEPROM_DATA_START + idx * p;
+  for(uint16_t k = 0; k<p; ++k ) {
+    EEPROM.write(j++, share[k+1]);
+  }
+
+  if(idx == shares) {
+    shares += 1;
+    EEPROM.write( SSSS_EEPROM_SHARE_IDX, shares);
+  }
+
+  return true;
+}
+  
+bool 
+Ssss::secretAvailable() {
+  return shares >= threshold;
+}
+
+void
+Ssss::beginReveal() {
+  mode = Reveal;
+}
+
+void 
+Ssss::getSecret(uint8_t *buffer) {
+  uint8_t p = payload + 1;
+  uint16_t k = SSSS_EEPROM_DATA_START;
+  for(uint8_t i=0; i<shares; ++i, k+=p) {
+    powers[i] = EEPROM.read(k);
+  }
+  
+  for(uint8_t i=0; i<shares; ++i) {
+    lagrange_value[i] = lagrange(shares, powers, i, 0);
+  }
+
+  for(uint8_t i =0; i<payload; i++) {
+    gf256 sum = 0;
+    k=SSSS_EEPROM_DATA_START+i;
+    for(uint8_t j = 0; j<shares; ++j, k+=p) {
+      sum = sum + lagrange_value[j] * EEPROM.read( k );
+    }
+    buffer[i] = sum.n;
+  } 
+}
+
---
+++ ssss.h
@@ -0,0 +1,135 @@
+#ifndef SSSS_H
+#define SSSS_H
+
+#include <gf256.h>
+
+
+#define SSSS_EEPROM_MODE_IDX    0
+#define SSSS_EEPROM_PAYLOAD_IDX 1
+#define SSSS_EEPROM_FAMILY_IDX  2
+#define SSSS_EEPROM_SHARE_IDX   3
+
+#define SSSS_EEPROM_DATA_START  8
+
+// NONE mode is when we have not started on a particular
+// operation yet. From NONE we can go to DEAL mode by
+// entering in the detals of the share configuration and
+// by entering in the secret to be shared.
+// From NONE mode, we can also enter COLLECT mode by entering
+// the first of a set of shares.
+#define SSSS_MODE_NONE    0
+
+// DEAL mode is where we have a secret stored in EEPROM
+// and the inderface will pass out successive shares.
+// When done dealing shares, the device gets wiped back to 
+// NONE mode
+#define SSSS_MODE_DEAL    1
+
+// In COLLECT mode we can see details of the share family,
+// required shares to recover, and the number of shares 
+// collected so far. We can also enter additional shares
+#define SSSS_MODE_COLLECT 2
+
+// When we have enough shares to reover the secret we go into
+// recover mode, which allows us to view the secret. Once the
+// secret has been revealed, the device can be wiped back to
+// NONE mode.
+#define SSSS_MODE_RECOVER 3
+
+
+enum SsssMode: uint8_t {
+  NoMode, Shuffle, Deal, Collect, Reveal
+};
+
+// Shares add two bytes to the beginning of the payload
+// representing the family id, the number of shares needed to rebuild,
+// and the share id.
+
+// All shares in the share set have a common familyId byte.
+// All shares in the share set have a unique id
+// No share should ever be created with the id 0
+
+struct FamilyId {
+  unsigned int family : 5;  // all shares for a given secret will be of the same family
+  unsigned int req    : 3;  // number of shares needed to build the secret
+};
+
+// EEPROM layout
+// [0] mode  
+// [1] payloadBytes 
+// [2] familyId
+// [3] current shareId (deal) / next share slot (collect)
+
+// deal mode
+// [4 --> (payloadBytes) +3 ] secret 
+// [4+payloadBytes -> 4+n*payloadBytes +3] entropy
+
+// collect mode
+// [4] oth shareid 
+// [5 ---> payloadBytes -+ 4 ] share payload
+
+#define SSSS_MAX_SHARES 16
+
+class Ssss {
+  private:
+  void setMode(uint8_t mode);  
+  void setShares(uint8_t collected);  
+  void saveShare(uint8_t shareIndex, uint8_t *share);
+  uint8_t getFamilyId() const;
+  
+  uint8_t ent_index;
+  uint8_t ent_sweeps;
+  uint8_t family;
+  uint8_t threshold;
+  uint8_t payload;
+  uint8_t shares;
+
+  gf256 powers[16];
+  gf256 lagrange_value[16];
+
+  public:
+  uint8_t mode;
+  
+  void LoadState();
+  uint8_t getShares() const;
+
+  // General Mode function
+  uint8_t getMode() const;  // get the mode
+  void clear(); // wipe the device and return to NONE mode
+  void setPayload(uint8_t payload);  
+  void setFamily(uint8_t fam);  
+  void setThreshold(uint8_t thresh);  
+
+  // common status functions
+  uint8_t getFamily() const {return family;}
+  uint8_t getThreshold() const {return threshold;}
+  uint8_t getPayloadBytes() const {return payload;}
+  bool hasFamily() const;
+
+  // Deal Setup
+  void beginShuffle();
+  void setPayloadLength(uint8_t payloadLength);
+  bool addEntropy(uint8_t entropy);
+  void addSecret(uint8_t *secret);
+
+  // Deal mode
+  void beginDeal();
+  void dealNextShare(int8_t *buffer); // writes share bytes to buffer - does not include checksum
+  uint8_t getCurrentShare() const {return shares;}
+
+  // Collect Mode Setup
+  void beginCollect();
+  uint8_t collectedSharesCount() const;
+  void setSharePayloadLength(uint16_t payloadLength);  
+
+  void addShare(uint8_t *share);  // this payload includes the family and share id...
+  bool hasShare(uint8_t *share) const; // check if the share we are about to add is already in the set
+  bool checkFamily(uint8_t *share) const; // check if the share id we are about to add is the right family
+
+  // Recover Mode
+  bool secretAvailable();
+  void beginReveal();
+  void getSecret(uint8_t *buffer); // copy secret into buffer
+};
+
+#endif
